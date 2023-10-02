0a57346db74b5e2853c4e6261b03f0d5419f7bce
Benelab <78307945+benesnpl@users.noreply.github.com>
2023-10-02 05:22:24 -0500

    Update S2S.tf

--- S2S.tf
+++ S2S.tf
@@ -1,3 +1,5 @@
+# Create Customer Gateway with oakbrook Public IP
+
 resource "aws_customer_gateway" "oakbrook" {
   bgp_asn                                       = 65000
   ip_address                                    = var.il_external
@@ -7,6 +9,8 @@ resource "aws_customer_gateway" "oakbrook" {
     Name                                        = join("", [var.coid, "-", var.aws_region, "-CGW"])
   }
   }
+
+# Create Oakbrook VPN pointing to TGW
 
 resource "aws_vpn_connection" "Oakbrook" {
   transit_gateway_id                            = aws_ec2_transit_gateway.main_tgw.id
@@ -18,11 +22,15 @@ resource "aws_vpn_connection" "Oakbrook" {
   }
 }
 
+#Get the ID of the newly created (auto) VPN TGW attachment
+
 data "aws_ec2_transit_gateway_vpn_attachment" "oak_attach" {
   depends_on                                    = [aws_vpn_connection.Oakbrook, aws_ec2_transit_gateway.main_tgw]
   transit_gateway_id                            = aws_ec2_transit_gateway.main_tgw.id
   vpn_connection_id                             = aws_vpn_connection.Oakbrook.id
 }
+
+#Give name Tag to VPN TGW attachment
 
 resource "aws_ec2_tag" "tag_vpn_attach" {
   resource_id = data.aws_ec2_transit_gateway_vpn_attachment.oak_attach.id
