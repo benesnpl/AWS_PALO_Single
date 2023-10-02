03c7b4f86ab4dcdddb69d01fc6c2a91ade9ada5c
Benelab <78307945+benesnpl@users.noreply.github.com>
2023-10-02 05:20:31 -0500

    Update IAM.tf

--- IAM.tf
+++ IAM.tf
@@ -1,3 +1,5 @@
+#Creates the ha policy for the DR implementation -- Name changes to *2 to avoid issues with PRD role
+
 resource "aws_iam_policy" "hapolicy" {
   name        = "hapalo2"
   path        = "/"
@@ -22,6 +24,8 @@ resource "aws_iam_policy" "hapolicy" {
   })
 }
 
+#Role creation based on the policy
+
 resource "aws_iam_role" "ec2_role" {
   name = "hapalo2"
   assume_role_policy = jsonencode ({
@@ -38,14 +42,17 @@ resource "aws_iam_role" "ec2_role" {
       ]
     })
   }
-  
+
+# Attach policy to Role
   
   resource "aws_iam_policy_attachment" "ec2_policy_role" {
     name  = "ec2_attachment_2"
     roles = [aws_iam_role.ec2_role.name]
     policy_arn = aws_iam_policy.hapolicy.arn
    }
-   
+
+#Create policy profile so TF can attach role to VM
+
   resource "aws_iam_instance_profile" "ec2_profile" {
     name = "ec2_profile_2"
     role = aws_iam_role.ec2_role.name
