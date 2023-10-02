425ed908a6e7d284b9367ed3a5d7d202385e9bc8
Benelab <78307945+benesnpl@users.noreply.github.com>
2023-10-02 04:35:14 -0500

    Update tgw-igw.tf

--- tgw-igw.tf
+++ tgw-igw.tf
@@ -12,8 +12,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-main" {
   depends_on                                            = [aws_subnet.TGW,aws_ec2_transit_gateway.main_tgw]
   subnet_ids                                            = "${aws_subnet.TGW.*.id}"
   transit_gateway_id                                    = aws_ec2_transit_gateway.main_tgw.id
-  transitGatewayDefaultRouteTableAssociation            = false
-  transitGatewayDefaultRouteTablePropagation            = true
+  transit_gateway_default_route_table_association       = false
+  transit_gateway_default_route_table_propagation       = false
   vpc_id                                                = aws_vpc.main_vpc.id
   appliance_mode_support                                = "enable"
   tags = {
